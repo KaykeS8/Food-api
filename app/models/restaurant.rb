e328b3fb6f50882801d4ccef3c7895ac3c026fb0
kayke <kayke.py@gmail.com>
2025-01-01 16:50:25 -0300

    Model referente a restaurant

---
+++ app/models/restaurant.rb
@@ -0,0 +1,14 @@
+class Restaurant < ApplicationRecord
+  belongs_to :category
+  has_many :product_categories
+  has_many :orders
+  validates :name, :delivery_tax, :city, :neighborhood, :street, :number, presence: true
+  has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "city", "complement", "created_at", "delivery_tax", "description", "id", "name", "neighborhood", "number", "street", "updated_at"]
+  end
+  def self.ransackable_associations(auth_object = nil)
+    ["category", "image_attachment", "image_blob", "orders", "product_categories"]
+  end
+end
